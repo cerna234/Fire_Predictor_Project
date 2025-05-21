1a0934431b747315bb2e33a7a7199c4ff4c8cac1
cerna234 <cerna2424@gmail.com>
2025-05-20 21:02:28 -0700

    Model update and api update

--- fire_prediction_Project_API/app.py
+++ fire_prediction_Project_API/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from joblib import load
 
 
@@ -7,46 +7,38 @@ from joblib import load
 app = Flask(__name__)
 
 
-regressor = load(r'C:\Users\Miguel Cerna\OneDrive\Desktop\Fire_Predictor_Project\RegressionModel\fire_predictor_pipeline.joblib')
-classifier = load(r'C:\Users\Miguel Cerna\OneDrive\Desktop\Fire_Predictor_Project\classifierModel\fire_predictor_pipeline_Classifier.joblib')
+regressor = load(r'/Users/miguelcerna/Desktop/Fire_Predictor_Project/fire_prediction_Project_API/Models/fire_predictor_regression_model.joblib')
+classifier = load(r'/Users/miguelcerna/Desktop/Fire_Predictor_Project/fire_prediction_Project_API/Models/fire_predictor_classifier_model.joblib')
 
 @app.route("/")
 def home():
     
     return "up and Running"
 
-@app.route("/fire_Predictor_regressor")
+@app.route("/fire_Predictor_regressor",methods=['GET', 'POST'])
 def fire_predictor_regressor():
-        # Load the pipeline
+
     
-
-    # New sample (must match the original column order and types)
-    sample = [[334.0961,	-117.5279,	301.4,	1	,1,	289.6	,3.3,	'D' ,	2	, 19,	171	, 'Degrees Compass'	,1]]  # 13 columns
-
-    # Predict
+    sample = request.get_json()
+    
     prediction = regressor.predict(sample)
-   
-
-
 
     print(prediction)
 
     return jsonify(prediction=prediction.tolist())
 
-@app.route("/fire_Predictor_Classifier")
+@app.route("/fire_Predictor_Classifier",methods=['GET', 'POST'])
 def fire_predictor_Classifier():
     
-
-    #update to take in input
-    sample = [[36.6853	,-119.6669,371.9,1,1,291.4	,124.2,'D'	,0,	21 ,	193.6,	'Degrees Compass'	,0.1], 
-            [33.9604,	-117.4206,317.2,1.5,1.2	,291.5,	22.5,'D'	,2	,18 ,	0.7	,'Knots',	0.1], 
-            [33.2066 ,-117.3809,303.1,1.1,1,289.6,4.8,'D',2,19,21	,'Degrees Compass',	0.1	]
-            ]  # 12 columns
-
-    # Predict
+    
+    sample = request.get_json()
+    
     prediction = classifier.predict(sample)
-
-
+    
     print(prediction)
 
     return jsonify(prediction=prediction.tolist())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
