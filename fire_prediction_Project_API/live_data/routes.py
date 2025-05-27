5fc936a8dfb2a85c6bfd34f7ebd37543c29a6a57
cerna234 <cerna2424@gmail.com>
2025-05-27 16:10:49 -0700

    Helper functions added

--- fire_prediction_Project_API/live_data/routes.py
+++ fire_prediction_Project_API/live_data/routes.py
@@ -12,8 +12,11 @@ live_data_bp= Blueprint('live_data', __name__)
 
 api_key = 'bcefad9a65f2489fb2f231456252205'
 
-@live_data_bp.route("/live_data", methods=['POST'])
-def live_data():
+
+# Helper Functions
+
+
+def live_data_helper(api_key,location):
 
     location = request.get_json()
     url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={location}&aqi=no'
@@ -47,8 +50,88 @@ def live_data():
             
         }
         
-        return jsonify(result)
+        return result
 
+
+
+
+def live_modis_data_helper(coordinates,date,date_range):
+
+    #west,south,east,northwest,south,east,north
+    #coordinates = "-125,24,-66,49"
+    #date = '1/2025-05-23'
+    #url = f'https://firms.modaps.eosdis.nasa.gov/api/area/csv/e3994c1efe51efb62bb18c25370bc03a/MODIS_NRT/{coordinates}/{date_range}/{date}'
+    url = f'https://firms.modaps.eosdis.nasa.gov/api/area/csv/e3994c1efe51efb62bb18c25370bc03a/MODIS_NRT/{coordinates}/{date_range}/{date}'
+    response = requests.get(url)
+
+    data = response.text
+
+
+    csv_file = io.StringIO(response.text)
+
+    dataset = pd.read_csv(csv_file)
+
+    random_row = dataset.sample(n=1).iloc[0]
+   
+    data = random_row
+    #brightness	scan	track	bright_t31	frp
+    
+
+    live_modis_data = {
+    'brightness': data['brightness'],     
+    'scan': data['scan'],                
+    'track': data['track'],               
+    'bright_t31': data['bright_t31'],    
+    'frp': data['frp']                    
+    }
+    
+    
+
+    
+    return live_modis_data
+
+
+#LIVE ROUTE FUNCTIONS
+@live_data_bp.route("/live_data", methods=['POST'])
+def live_data():
+
+    location = request.get_json()
+    
+    data = live_data_helper(api_key,location)
+    
+    return jsonify(data)
+
+
+@live_data_bp.route("/live_Modis_data", methods=['POST'])
+def live_modis_data():
+
+    #west,south,east,northwest,south,east,north
+
+    
+  
+
+    #coordinates = "-125,24,-66,49"
+    #date = '1/2025-05-23'
+
+    json_data = request.get_json()
+
+    coordinates = json_data.get("coordinates")
+    date = json_data.get("date")
+    date_range = json_data.get("date_range")
+
+
+    data = live_modis_data_helper(coordinates,date,date_range)
+
+    print(data)
+
+
+
+    
+    return jsonify(data)
+
+
+
+#FUTURE DATA ROUTE FUNCTIONS
 
 @live_data_bp.route("/future_data", methods=['POST'])
 def future_data():
@@ -87,45 +170,3 @@ def future_data():
     
     
     return jsonify(result)
-
-
-@live_data_bp.route("/live_Modis_data", methods=['POST'])
-def live_modis_data():
-
-    #west,south,east,northwest,south,east,north
-    coordinates = "-125,24,-66,49"
-    date = '1/2025-05-23'
-    url = f'https://firms.modaps.eosdis.nasa.gov//api/area/csv/e3994c1efe51efb62bb18c25370bc03a/MODIS_NRT/{coordinates}/{date}'
-    
-    response = requests.get(url)
-
-    data = response.text
-
-
-    csv_file = io.StringIO(response.text)
-
-    dataset = pd.read_csv(csv_file)
-
-    random_row = dataset.sample(n=1).iloc[0]
-   
-    data = random_row
-    #brightness	scan	track	bright_t31	frp
-    live_modis_data = {
-    'brightness': data['brightness'],     
-    'scan': data['scan'],                
-    'track': data['track'],               
-    'bright_t31': data['bright_t31'],    
-    'frp': data['frp']                    
-    }
-    
-
-    print(live_modis_data)
-
-  
-
-
-
-
-
-    
-    return live_modis_data
